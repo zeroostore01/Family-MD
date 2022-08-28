994cd6f97332d0beaa6b6dfb043fb428164a3204
zeroostore01 <107806866+zeroostore01@users.noreply.github.com>
2022-08-28 21:58:12 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -11,7 +11,7 @@ handler.before = async function (m, { isAdmin, isBotAdmin }) {
     if (m.text.includes(thisGroup)) throw false // jika link grup itu sendiri gak dikick
       if (!isBotAdmin) m.reply(` *「 ANTILINK 」* ${isAdmin ? "Admin mah bebas ygy :'v" : `\n\nlink group terdeteksi dan ${global.namabot} bukan admin jadi tidak bisa ngekick!`}`)
     if (isBotAdmin) {
-      m.reply(` *「 ANTILINK 」* \n\nLink Group Terdeteksi, bye Kamu Akan Di Kick!!`.trim())
+      m.reply(` *「 ANTILINK 」* \n\nCiee Sharelink Miskin Member ya v:`.trim())
       await this.delay(500)
       await this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
     }
